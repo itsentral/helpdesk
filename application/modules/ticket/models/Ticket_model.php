12e7f771941a4435af043f8605d8186c092d2010
mocheru <herunoor09@gmail.com>
2026-04-06 15:57:36 +0700

    debug

--- application/modules/ticket/models/Ticket_model.php
+++ application/modules/ticket/models/Ticket_model.php
@@ -250,7 +250,7 @@ class Ticket_model extends BF_Model
 
     public function insert_ticket($data)
     {
-        $this->db->insert('helpdesk', $data);
+        $this->db->insert('helpdeska', $data);
 
         return $this->db->insert_id();
     }
